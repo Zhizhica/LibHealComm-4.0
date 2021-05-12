35042c23d5f0cddde0c389ebd5efd305acdcd873
Zhizhica <darenei900@mai.ru>
2021-05-12 18:25:49 +0300

    change spellID acquiring from combat log for TBC

--- LibHealComm-4.0.lua
+++ LibHealComm-4.0.lua
@@ -2160,9 +2160,11 @@ function HealComm:COMBAT_LOG_EVENT_UNFILTERED(...)
 	local timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
 	if( not eventRegistered[eventType] ) then return end
 
-	local _, spellName = select(12, ...)
+	local spellID, spellName = select(12, ...)
 	local destUnit = guidToUnit[destGUID]
-	local spellID = destUnit and select(10, unitHasAura(destUnit, spellName)) or select(7, GetSpellInfo(spellName))
+	if not isTBC then
+		spellID = destUnit and select(10, unitHasAura(destUnit, spellName)) or select(7, GetSpellInfo(spellName))
+	end
 
 	-- Heal or hot ticked that the library is tracking
 	-- It's more efficient/accurate to have the library keep track of this locally, spamming the comm channel would not be a very good thing especially when a single player can have 4 - 8 hots/channels going on them.
